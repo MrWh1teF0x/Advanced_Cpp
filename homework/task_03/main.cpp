0af69b0e0ba2d941f1aa6daf186bea88ed82702b
MrWh1teF0x <akramovnekit@mail.ru>
2025-05-14 23:55:20 +0300

    task_03: complete

--- homework/task_03/main.cpp
+++ homework/task_03/main.cpp
@@ -1,1 +1,56 @@
-int main() { return 0; }
+#include <iostream>
+
+#include "src/typemap.hpp"
+
+struct DataA {
+  std::string value;
+};
+
+struct DataB {
+  int value;
+};
+
+int main() {
+  TypeMap<int, DataA, double, DataB> myTypeMap;
+
+  // Добавление элементов в контейнер
+  myTypeMap.AddValue<int>(42);
+  myTypeMap.AddValue<double>(3.14);
+  myTypeMap.AddValue<DataA>({"Hello, TypeMap!"});
+  myTypeMap.AddValue<DataB>({10});
+
+  static_assert(myTypeMap.Contains<int>() == true,
+                "myTypeMap.Contains<int>() == true");
+  static_assert(myTypeMap.Contains<double>() == true,
+                "myTypeMap.Contains<double>() == true");
+  static_assert(myTypeMap.Contains<DataA>() == true,
+                "myTypeMap.Contains<DataA>() == true");
+  static_assert(myTypeMap.Contains<DataB>() == true,
+                "myTypeMap.Contains<DataB>() == true");
+  static_assert(myTypeMap.Contains<std::string>() == false,
+                "myTypeMap.Contains<std::string>() == false");
+  static_assert(myTypeMap.Contains<std::tuple<int>>() == false,
+                "myTypeMap.Contains<std::tuple<int>>() == false");
+
+  // Получение и вывод значений по типам ключей
+  std::cout << "Value for int: " << myTypeMap.GetValue<int>()
+            << std::endl;  // Вывод: 42
+  std::cout << "Value for double: " << myTypeMap.GetValue<double>()
+            << std::endl;  // Вывод: 3.14
+  std::cout << "Value for DataA: " << myTypeMap.GetValue<DataA>().value
+            << std::endl;  // Вывод: Hello, TypeMap!
+  std::cout << "Value for DataB: " << myTypeMap.GetValue<DataB>().value
+            << std::endl;  // Вывод: 10
+  // Проверка наличия элемента
+  std::cout << "Contains int? " << (myTypeMap.Contains<int>() ? "Yes" : "No")
+            << std::endl;  // Вывод: Yes
+
+  // Удаление элемента
+  myTypeMap.RemoveValue<double>();
+
+  // Попытка получения удаленного элемента
+  std::cout << "Value for double after removal: "
+            << myTypeMap.GetValue<double>()
+            << std::endl;  // Вывод: (некорректное значение)
+  return 0;
+}
---
+++ homework/task_03/src/typemap.hpp
@@ -0,0 +1,35 @@
+#include "typelist.hpp"
+
+template <typename... Types>
+class TypeMap {
+ public:
+  template <typename T>
+  void AddValue(const T& value) {
+    static_assert(typelist::Contains<T, Keys>::value,
+                  "Type doesn't exist in TypeMap!");
+    std::get<typelist::IndexOf<T, Keys>::index>(values_) = value;
+  }
+
+  template <typename T>
+  const T& GetValue() const {
+    static_assert(typelist::Contains<T, Keys>::value,
+                  "Type doesn't exist in TypeMap!");
+    return std::get<typelist::IndexOf<T, Keys>::index>(values_);
+  }
+
+  template <typename T>
+  static constexpr bool Contains() {
+    return typelist::Contains<T, Keys>::value;
+  }
+
+  template <typename T>
+  void RemoveValue() {
+    static_assert(typelist::Contains<T, Keys>::value,
+                  "Type doesn't exist in TypeMap!");
+    std::get<typelist::IndexOf<T, Keys>::index>(values_) = T{};
+  }
+
+ private:
+  using Keys = typelist::TypeList<Types...>;
+  std::tuple<Types...> values_;
+};
